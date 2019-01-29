dc59abf7ac750e5cc6e87827fb7d53792ecb4f9a
AeDani <31882578+AeDani@users.noreply.github.com>
2019-01-29 21:59:17 +0100

    Test POST routes

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json())
 
 // Routes
 app.post('/todos', (req, res) => {
-    console.log(req.body)
+    // console.log(req.body)
     var todo = new Todo({
         text: req.body.text
     })
@@ -27,3 +27,6 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
     console.log('Started on port 3000')
 })
+
+
+module.exports = {app}
