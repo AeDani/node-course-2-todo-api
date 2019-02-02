1f95ba2384b71b61eca63c1d6bdef51e806a2690
AeDani <31882578+AeDani@users.noreply.github.com>
2019-02-02 21:07:03 +0100

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express')
-var bodyParser = require('body-parser')
+const _ = require('lodash')
+const express = require('express')
+const bodyParser = require('body-parser')
 const {ObjectId} = require('mongodb')
 
 var {mongoose} = require('./db/mongoose')
@@ -85,6 +86,34 @@ app.delete('/todos/:id', (req, res) => {
 
     }, (e) => {
         // error -> return 404
+        res.status(404).send()
+    })
+})
+
+
+// PATCH route
+app.patch('/todos/:id', (req,res)=> {
+    var id = req.params.id
+    var body = _.pick(req.body, ['text', 'completed'])
+
+    if(!ObjectId.isValid(id)){
+        // Status 404 - empty body
+        return res.status(404).send('not valid id')
+    }
+
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime()
+    } else {
+        body.completed = false
+        body.completedAt = null
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo)=> {
+        if(!todo){
+            return res.status(404).send('id not found')
+        }
+        res.send({todo})
+    }).catch((e) => {
         res.status(404).send()
     })
 })
