0873f2923068d09969639a046ed31e560a17e7dc
AeDani <31882578+AeDani@users.noreply.github.com>
2019-02-02 20:31:08 +0100

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -37,7 +37,6 @@ app.get('/todos', (req, res) => {
 })
 
 // GET one todo by id
-
 app.get('/todos/:id', (req, res) => {
     var id = req.params.id
     // res.send(id)
@@ -59,6 +58,34 @@ app.get('/todos/:id', (req, res) => {
     }, (e) => {
         // error - status 400 - empty body
         res.status(400).send()
+    })
+})
+
+
+// DELETE route
+app.delete('/todos/:id', (req, res) => {
+    // get the id
+    var id = req.params.id
+
+    // validate the id 
+    if (!ObjectId.isValid(id)) {
+        // not valid id -> return 404
+        return res.status(404).send('not valid id')
+    }
+
+    // find remove by id
+    Todo.findByIdAndRemove(id).then((todo) => {
+        // sucess
+        if (!todo) {
+            // if no doc gets back -> return 404
+            return res.status(404).send('no doc to remove')
+        }
+        // if removed -> return doc
+        res.send({ todo })
+
+    }, (e) => {
+        // error -> return 404
+        res.status(404).send()
     })
 })
 
