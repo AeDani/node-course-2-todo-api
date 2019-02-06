ed2086a2968c85f48a4e8da6852ede8aedcb84cd
AeDani <31882578+AeDani@users.noreply.github.com>
2019-02-06 21:08:58 +0100

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -122,9 +122,25 @@ app.patch('/todos/:id', (req,res)=> {
 })
 
 
+// POST a new User
+app.post('/users', (req, res) => {
+    // read body from request
+    var body = _.pick(req.body, ['email', 'password'])
+    // create instance of user
+    var user = new User(body)
+    // save user to db
+    user.save().then(() => {
+        return user.generateAuthToken()
+    }).then((token) => {
+        res.header('x-auth',token).send(user)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
+
+
 app.listen(port, () => {
     console.log(`Started on port ${port}`)
 })
-
 
 module.exports = {app}
